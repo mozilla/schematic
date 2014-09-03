a3adf69738a2d99b718be35eef58addd5857e11c
Yohan Boniface <yb@enix.org>
2014-09-03 17:42:44 +0200

    Add --fake parameter (bug 1061143)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='schematic',
-    version='0.2',
+    version='0.3',
     url='https://github.com/mozilla/schematic',
     author_email='dev-webdev@lists.mozilla.org',
     scripts=['schematic'],
