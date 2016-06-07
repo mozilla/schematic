bb74079426adb0e0419c8037e6d448a23e76a13f
Andy McKay <amckay@mozilla.com>
2016-06-07 08:05:57 -0700

    new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='schematic',
-    version='0.3',
+    version='0.4',
     url='https://github.com/mozilla/schematic',
     author_email='dev-webdev@lists.mozilla.org',
     scripts=['schematic'],
